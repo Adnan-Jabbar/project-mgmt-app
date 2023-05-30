e179711f8f73685eb82d70f78d5b3836bd5c924b
Adnan Jabbar <adnan.jabbar@m3tech.com.pk>
2023-05-31 00:42:14 +0500

    Configuration updates and add color library updates few method in GraphQL schema

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,17 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI);
+    
+    // const conn = await mongoose.connect(process.env.MONGO_URI, {
+    //     poolSize: 10,
+    //     authSource: "admin",
+    //     user: "admin",
+    //     pass: "password",
+    //     useNewUrlParser: true,
+    //     useUnifiedTopology: true,
+    //     useCreateIndex: true,
+    //     useFindAndModify: false 
+    // });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
 }
